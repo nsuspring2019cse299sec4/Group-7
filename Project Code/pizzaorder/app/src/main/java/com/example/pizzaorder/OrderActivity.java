47154a7527393bc5131591c9c7ae0f0880cc2aff
LimonMotin <47325340+LimonMotin@users.noreply.github.com>
2019-03-10 01:21:46 +0600

    Updated OrderActivity.Java
    onClick(View v) method implemented  according to Button Click.

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/OrderActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/OrderActivity.java
@@ -1,6 +1,8 @@
 package com.example.pizzaorder;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +12,7 @@ import android.widget.TextView;
 
 public class OrderActivity extends ListActivity implements View.OnClickListener{
 
-
+    public boolean hasShown = false;
     public TextView Total;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,31 @@ public class OrderActivity extends ListActivity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.newPizza:
+                if (!hasShown) {
+                    openHowToDialog();
+                    hasShown = true;
+                } else {
+                    openSizeSelectionDialog();
+                }
+                break;
+            case R.id.checkout:
+                if (getListView().getCount() != 0)
+                    checkOutDialog();
+                else {
+                    new AlertDialog.Builder(this).setTitle("Info").setMessage("You must order a pizza, before you can checkout.").setCancelable(false)
+                            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    openSizeSelectionDialog();
+                                }
+                            }).show();
+                }
+                break;
+        }
 
     }
 }
