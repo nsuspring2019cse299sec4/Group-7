9b5eae13a572d807bedb987199e719e71ae4f263
LimonMotin <47325340+LimonMotin@users.noreply.github.com>
2019-03-10 01:13:22 +0600

    Updated OrderActivity.Java
    Initial coding structure implemented.

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/OrderActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/OrderActivity.java
@@ -1,13 +1,45 @@
 package com.example.pizzaorder;
 
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends ListActivity implements View.OnClickListener{
 
+
+    public TextView Total;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        getListView().setChoiceMode(1);
+
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onNothingSelected(AdapterView arg0) {
+
+            }
+
+            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
+
+            }
+
+        });
+
+        View getNewPizzaButton = findViewById(R.id.newPizza);
+        getNewPizzaButton.setOnClickListener(this);
+        View getCheckoutButton = findViewById(R.id.checkout);
+        getCheckoutButton.setOnClickListener(this);
+        Total = (TextView) findViewById(R.id.total);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
