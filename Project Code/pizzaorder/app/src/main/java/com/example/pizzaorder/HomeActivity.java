6b839cf57ce372b308479495de047b33916f91a6
LimonMotin <47325340+LimonMotin@users.noreply.github.com>
2019-03-10 00:46:43 +0600

    OrderActivity.Java and activity_order.xml File have been added!
    In the interface design, just TextView and ListView field added initially!

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/HomeActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/HomeActivity.java
@@ -1,23 +1,16 @@
 package com.example.pizzaorder;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.pizzaorder.common.Common;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -55,7 +48,7 @@ public class HomeActivity extends AppCompatActivity {
 
             public void onClick (View view){
 
-                Intent intent = new Intent(HomeActivity.this, PizzaSizeActivity.class);
+                Intent intent = new Intent(HomeActivity.this, OrderActivity.class);
                 startActivity(intent);
             }
         });
--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/OrderActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/OrderActivity.java
@@ -3,11 +3,11 @@ package com.example.pizzaorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PizzaSizeActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pizza_size);
+        setContentView(R.layout.activity_order);
     }
 }
