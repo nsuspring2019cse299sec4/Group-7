3267f06f86d6ad22ffe72c7cc5d2f57e272de65e
Md. Golam Rabby Shuvo <47330526+shuuuuvo@users.noreply.github.com>
2019-04-01 03:09:42 +0600

    Update NewPizzaActivity.java
    Button handled and structure updated.

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/NewPizzaActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/NewPizzaActivity.java
@@ -1,13 +1,55 @@
 package com.example.pizzaorder;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class NewPizzaActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    @Override
+public class NewPizzaActivity extends Activity implements View.OnClickListener {
+
+    public RadioButton wholeRadio;
+    public RadioButton leftRadio;
+    public RadioButton rightRadio;
+    public TextView wholeText;
+    public TextView leftText;
+    public TextView rightText;
+    public ArrayList<String> wList = new ArrayList<String>();
+    public ArrayList<String> lList = new ArrayList<String>();
+    public ArrayList<String> rList = new ArrayList<String>();
+    public ArrayList<String> topingList = new ArrayList<String>();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_pizza);
+
+        wholeRadio = (RadioButton) findViewById(R.id.whole);
+        leftRadio = (RadioButton) findViewById(R.id.left);
+        rightRadio = (RadioButton) findViewById(R.id.right);
+        wholeText = (TextView) findViewById(R.id.whole_text);
+        leftText = (TextView) findViewById(R.id.left_text);
+        rightText = (TextView) findViewById(R.id.right_text);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_to_cart_button:
+                addToCart();
+                finish();
+                break;
+            case R.id.cancel_button:
+                finish();
+                break;
+        }
+    }
+
+    private void addToCart() {
+
     }
 }
+
+
