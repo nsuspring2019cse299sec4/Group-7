3d35f3a273a868d6593e02d41b62ff9960395b7f
LimonMotin <47325340+LimonMotin@users.noreply.github.com>
2019-04-01 01:13:31 +0600

    Update PizzaData.java

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/PizzaData.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/PizzaData.java
@@ -1,57 +1,14 @@
 package com.example.pizzaorder;
 
-public class PizzaData {
-    private String Size;
-    private String Crust;
-    private String topWhole;
-    private String topLeft;
-    private String topRight;
+import android.provider.BaseColumns;
 
-    public PizzaData(String size, String crust, String topWhole, String topLeft, String topRight) {
-        Size = size;
-        Crust = crust;
-        this.topWhole = topWhole;
-        this.topLeft = topLeft;
-        this.topRight = topRight;
-    }
+public interface PizzaData extends BaseColumns {
+    public static final String TABLE_NAME = "pizza";
 
-    public String getSize() {
-        return Size;
-    }
-
-    public void setSize(String size) {
-        Size = size;
-    }
-
-    public String getCrust() {
-        return Crust;
-    }
-
-    public void setCrust(String crust) {
-        Crust = crust;
-    }
-
-    public String getTopWhole() {
-        return topWhole;
-    }
-
-    public void setTopWhole(String topWhole) {
-        this.topWhole = topWhole;
-    }
-
-    public String getTopLeft() {
-        return topLeft;
-    }
-
-    public void setTopLeft(String topLeft) {
-        this.topLeft = topLeft;
-    }
-
-    public String getTopRight() {
-        return topRight;
-    }
-
-    public void setTopRight(String topRight) {
-        this.topRight = topRight;
-    }
+    // Columns in the Events database
+    public static final String SIZE = "size";
+    public static final String CRUST = "crust";
+    public static final String TOPPINGS_WHOLE = "toppingsWhole";
+    public static final String TOPPINGS_LEFT = "toppingsLeft";
+    public static final String TOPPINGS_RIGHT = "toppingsRight";
 }
