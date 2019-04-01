58d8608973f7a21b7b850bb8c33453dc3dff62ce
Md. Golam Rabby Shuvo <47330526+shuuuuvo@users.noreply.github.com>
2019-04-02 01:32:19 +0600

    Update NewPizzaActivity.java & Merge Problem
    Implemented addToCart() method.

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/NewPizzaActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/NewPizzaActivity.java
@@ -1,9 +1,10 @@
 package com.example.pizzaorder;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -32,6 +33,26 @@ public class NewPizzaActivity extends Activity implements View.OnClickListener {
         wholeText = (TextView) findViewById(R.id.whole_text);
         leftText = (TextView) findViewById(R.id.left_text);
         rightText = (TextView) findViewById(R.id.right_text);
+
+        Gallery gallery = (Gallery) findViewById(R.id.gallery);
+        gallery.setAdapter(new ImageAdapter(this));
+
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressWarnings("rawtypes")
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                if (wList.isEmpty())
+                    wList.clear();
+                if (lList.isEmpty())
+                    lList.clear();
+                if (rList.isEmpty())
+                    rList.clear();
+                }
+        });
+
+        View AddToCartButton = findViewById(R.id.add_to_cart_button);
+        AddToCartButton.setOnClickListener(this);
+        View CancelButton = findViewById(R.id.cancel_button);
+        CancelButton.setOnClickListener(this);
     }
 
     @Override
@@ -48,6 +69,25 @@ public class NewPizzaActivity extends Activity implements View.OnClickListener {
     }
 
     private void addToCart() {
+        String toppingsWhole = "";
+        String toppingsLeft = "";
+        String toppingsRight = "";
+        if (wList.size() > 0) {
+            toppingsWhole = toppingsWhole + editString(wList);
+        }
+        if (lList.size() > 0) {
+            toppingsLeft = toppingsLeft + editString(lList);
+        }
+        if (rList.size() > 0) {
+            toppingsRight = toppingsRight + editString(rList);
+        }
+        updatePizza(toppingsWhole, toppingsLeft, toppingsRight);
+    }
+
+    private String editString(ArrayList<String> wList) {
+    }
+
+    private void updatePizza(String toppingsWhole, String toppingsLeft, String toppingsRight) {
 
     }
 }
