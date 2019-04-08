16821ba3ea81035db759bda7110b0863e68c072f
Md. Golam Rabby Shuvo <47330526+shuuuuvo@users.noreply.github.com>
2019-04-09 04:00:15 +0600

    Merge Problem

--- Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/PizzaInfoActivity.java
+++ Project Code/pizzaorder/app/src/main/java/com/example/pizzaorder/PizzaInfoActivity.java
@@ -1,13 +1,21 @@
 package com.example.pizzaorder;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class PizzaInfoActivity extends AppCompatActivity {
+public class PizzaInfoActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza_info);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
